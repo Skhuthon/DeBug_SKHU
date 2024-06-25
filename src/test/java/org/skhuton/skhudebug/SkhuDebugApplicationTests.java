ea7c1bbf1c65a961dcf255e85cb33f5cec4eeec3
Hyun jinyong <ekwls20@gmail.com>
2024-06-25 13:37:37 +0900

    project modified

---
+++ src/test/java/org/skhuton/skhudebug/SkhuDebugApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skhuton.skhudebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkhuDebugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
