978ba9a142e6eba3e5f5b26bbdadae44ad282a05
zzdh8 <ekwls20@gmail.com>
2024-06-26 15:17:36 +0900

    test: 테스트 코드 주석처리 이번 프로젝트에서는 TDD를 채택하지 않았기 때문에 CI CD 파이프라인 구축을 위한 테스트 코드를 주석처리하였음.

--- src/test/java/org/skhuton/skhudebug/SkhuDebugApplicationTests.java
+++ src/test/java/org/skhuton/skhudebug/SkhuDebugApplicationTests.java
@@ -1,13 +1,13 @@
-package org.skhuton.skhudebug;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SkhuDebugApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package org.skhuton.skhudebug;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SkhuDebugApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
