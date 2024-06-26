8ce0d44bd72d6737cabe4805e9c61d9134d92597
zzdh8 <ekwls20@gmail.com>
2024-06-26 15:10:17 +0900

    chore: 일부 주석 삭제 불필요한 SwaggerConfig와 SecurityConfig 주석문 삭제

--- src/main/java/org/skhuton/skhudebug/config/SecurityConfig.java
+++ src/main/java/org/skhuton/skhudebug/config/SecurityConfig.java
@@ -40,11 +40,8 @@ public class SecurityConfig extends Exception {
                 .cors(c -> {
                             CorsConfigurationSource source = request -> {
                                 var cors = new CorsConfiguration();
-                                //허용할 origin
                                 cors.addAllowedOriginPattern("*");
-                                //허용할 method(CRUD)
                                 cors.setAllowedMethods(List.of("*"));
-                                //허용할 헤더
                                 cors.setAllowedHeaders(List.of("*"));
                                 cors.setAllowCredentials(true);
                                 cors.setMaxAge(3600L);
@@ -59,30 +56,29 @@ public class SecurityConfig extends Exception {
                         .requestMatchers("/**").permitAll()
                 )
                 .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)//필요 시 세션 생성
-                        .maximumSessions(5) // 동시 접속 가능 세션 오직 1
-                        .maxSessionsPreventsLogin(false) //로그인 시도 시 현재 접속시도자 인증 실패
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(5)
+                        .maxSessionsPreventsLogin(false)
                         .sessionRegistry(sessionRegistry())
                 )
                 //form login 설정
                 .formLogin((formLogin) ->
-                        formLogin //Postman으로 테스트 시 raw가 아닌 form-data로 해야 한다.
+                        formLogin
                                 .usernameParameter("loginId")
                                 .passwordParameter("password")
-                                .loginProcessingUrl("/user/login") //로그인 경로
-                                .successHandler(customAuthenticationSuccessHandler) //로그인 성공 시 핸들러
-                                .failureHandler(customAuthenticationFailureHandler)) //로그인 실패 시 핸들러
+                                .loginProcessingUrl("/user/login")
+                                .successHandler(customAuthenticationSuccessHandler)
+                                .failureHandler(customAuthenticationFailureHandler))
                 .logout((logout) -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
-                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK)) //로그아웃 성공 시 핸들러
-                        .addLogoutHandler(clearSiteData) //로그아웃 시 캐시 삭제
-                        .invalidateHttpSession(true)); // 로그아웃 시 인증정보 삭제 및 세션 무효화
+                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+                        .addLogoutHandler(clearSiteData)
+                        .invalidateHttpSession(true));
 
         return http.build();
     }
 
     @Bean
-    //스프링 시큐리티의 인증을 담당, 사용자 인증 시 UserService와 PasswordEncoder 사용
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
