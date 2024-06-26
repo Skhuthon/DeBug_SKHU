6d1bf461d6ca9d77a54ae393a5c5d536f58dd73a
zzdh8 <ekwls20@gmail.com>
2024-06-27 01:33:42 +0900

    refactor: 응답 객체 변경 ResponseEntity에서 BaseResponse로 변경함. SuccessCode에 200 상태코드와 메세지를 추가하였음.

--- src/main/java/org/skhuton/skhudebug/rank/controller/RankController.java
+++ src/main/java/org/skhuton/skhudebug/rank/controller/RankController.java
@@ -1,6 +1,9 @@
 package org.skhuton.skhudebug.rank.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.skhuton.skhudebug.common.dto.BaseResponse;
+import org.skhuton.skhudebug.exception.SuccessCode;
 import org.skhuton.skhudebug.member.domain.User;
 import org.skhuton.skhudebug.member.repository.UserRepository;
 import org.skhuton.skhudebug.rank.domain.HuntTime;
@@ -23,11 +26,13 @@ public class RankController {
     private final HuntTimeRepository huntTimeRepository;
     private final UserRepository userRepository;
 
+    @Operation(summary = "랭킹 조회", description = "유저의 벌레잡기 횟수를 기반으로 매긴 순위")
     @GetMapping("/rank")
-    public ResponseEntity<List<HuntTimeRanking>> getRanking() {
-        return new ResponseEntity<>(rankingService.getRanking(), HttpStatus.OK);
+    public BaseResponse<List<HuntTimeRanking>> getRanking() {
+        return BaseResponse.success(SuccessCode.RANKING_GET_SUCCESS, rankingService.getRanking());
     }
 
+    @Operation(summary = "test 전용 랭킹 Dummy Data 생성", description = "테스트를 위한 더미 데이터")
     @PostMapping("/rank/dummy")
     public ResponseEntity<Void> createDummyData(Authentication authentication) {
         User user = userRepository.findByLoginId(authentication.getName()).get();
