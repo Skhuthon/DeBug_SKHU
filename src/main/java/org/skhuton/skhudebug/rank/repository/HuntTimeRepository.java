1b0ca380ab64f1c37f322298cf4bb6b00e33b2fd
Hyun jinyong <ekwls20@gmail.com>
2024-06-30 00:08:10 +0900

    refactor: 헌트 횟수 증가 로직 추가(#8) - user의 loginId를 기준으로 테이블 탐색하는 메소드 추가 - 헌트를 완료한 유저의가 헌트 횟수 테이블에 있다면 헌트 횟수 증가, 없다면 횟수를 1로 초기화하고 저장

--- src/main/java/org/skhuton/skhudebug/rank/repository/HuntTimeRepository.java
+++ src/main/java/org/skhuton/skhudebug/rank/repository/HuntTimeRepository.java
@@ -7,12 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface HuntTimeRepository extends JpaRepository<HuntTime, Long> {
     @Query("SELECT new org.skhuton.skhudebug.rank.dto.HuntTimeRankingImpl(h.user.nickname, h.times) FROM HuntTime h ORDER BY h.times DESC")
     List<HuntTimeRanking> findByOrderByTimesDesc();
-
-    Optional<HuntTime> findByUser_LoginId(String loginId);
 }
